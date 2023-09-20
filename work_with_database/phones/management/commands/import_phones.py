dfb195fe4379fd77fd28b23f234a70ff3d0dbcc4
Marina Shtareva <marina.siar@email.ru>
2023-09-20 12:30:48 +0300

    Add

---
+++ work_with_database/phones/management/commands/import_phones.py
@@ -0,0 +1,23 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--phone', dest='phone', action='store')
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as file:
+            phones = list(csv.DictReader(file, delimiter=';'))
+
+        for phone in phones:
+            phones_data = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+            )
+            phones_data.save()
